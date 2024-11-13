bd4a734fbef9af9ab2fd4ae6b9f48421585fcfbb
Geng Liu <lucaso@uan-0002.head.cm.americas.sgi.com>
2024-11-13 18:13:43 +0000

    updated with profiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include<Kokkos_Core.hpp>
+#include <Kokkos_Core.hpp>
 #include<cstdio>
-#include "KokkosHeader.hh"
-
+#include <chrono>
 #include <ctime>
+#include <iostream>
+
+
+#define MyBackend Kokkos::OpenMP
+#define MyDeviceMemorySpace Kokkos::HostSpace
+#define MyHostMemorySpace Kokkos::HostSpace
+#define MyUSpace Kokkos::HostSpace
+
+#define MyView(Type) Kokkos::View<Type *, MyDeviceMemorySpace>
+#define MyUnmanagedHostView(Type) Kokkos::View<Type *, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+#define MyUnmanagedView(Type) Kokkos::View<Type *, MyDeviceMemorySpace, Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+
+
 
 int main(int argc, char* argv[]) {
   Kokkos::initialize(argc,argv);
@@ -12,7 +24,7 @@ int main(int argc, char* argv[]) {
    int VertPerCell=642;
    int size=Ncell*VertPerCell;
    double *array = (double*)malloc(size*sizeof(double));
-
+   MyView(double) reuseableTempMemory("reusableTempMemory",size);
    for (int i = 0; i < size; ++i) {
         array[i] = double(rand() % 100)/100.0;
    }
@@ -24,20 +36,32 @@ int main(int argc, char* argv[]) {
    Kokkos::deep_copy(arrayDeviceView,arrayView);
 
 
-   int *cellDel=(int*)malloc(100*sizeof(int));
-   for (int i=0; i<100; ++i){
-	cellDel[i] = rand() % Ncell + 1;
-   }
+//   int *cellDel=(int*)malloc(100*sizeof(int));
+//   for (int i=0; i<100; ++i){
+//	cellDel[i] = rand() % Ncell + 1;
+//   }
+   
+   auto start = std::chrono::high_resolution_clock::now();   
+   for (int i=0; i<2030; ++i){//about 2k cells being deleted in HARVEY
+        int nIndsToDelete = 642; //same as nVertsToDelete
+        int startIndex = 0; // same as startVertex
+        int preNIndices = Ncell*VertPerCell; //same as totalNVertices
+        int nDataToMove= preNIndices - (startIndex + nIndsToDelete);//10000000;//(Ncell-cellDel[i])*VertPerCell;
+	double* dstPtr = arrayDeviceView.data()+startIndex;
+	double* srcPtr = arrayDeviceView.data()+startIndex + nIndsToDelete;
+	double* tmpBufferPtr = reuseableTempMemory.data();
+        MyUnmanagedView(double) tmpBufferPtrView((double*)tmpBufferPtr,nDataToMove);
+        MyUnmanagedView(double) dstPtrView(dstPtr,nDataToMove);
+        MyUnmanagedView(double) srcPtrView(srcPtr,nDataToMove);
+        Kokkos::deep_copy(tmpBufferPtrView,srcPtrView);
+        Kokkos::deep_copy(dstPtrView,tmpBufferPtrView);
+	}
+   auto end = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> duration = end - start;
+   double timeInSeconds = duration.count();
 
-   for (int i=0; i<100; ++i){
-	int tmpSize=(Ncell-cellDel[i])*VertPerCell;
-	MyView(double) tmp("tmp",tmpSize);
-	MyUnmanagedView(double) src(arrayDeviceView.data()+cellDel[i]*VertPerCell,tmpSize);
-	MyUnmanagedView(double) dst(arrayDeviceView.data()+(cellDel[i]-1)*VertPerCell,tmpSize);
-	Kokkos::deep_copy(tmp,src);
-	Kokkos::deep_copy(dst,tmp);
-   }
-
+   std::cout << "Runtime (sec): " << timeInSeconds << std::endl;
+   free(array);
   }
   Kokkos::finalize();
 }
