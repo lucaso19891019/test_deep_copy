8d1c39f21999eb3af6a17d484a4ffd895e8f8382
Geng Liu <lucaso@uan-0002.head.cm.americas.sgi.com>
2024-11-13 18:26:47 +0000

    sycl version

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-export Kokkos_DIR=$PWD/install/kokkos/lib64/cmake/Kokkos/
-
 cmake -S src -B build/test \
- -D Kokkos_DIR=$Kokkos_DIR \
  -D CMAKE_CXX_FLAGS="-Wno-deprecated-declarations" \
  -D CMAKE_C_FLAGS="-Wno-stringop-overflow" \
  -D CMAKE_CXX_COMPILER=mpicxx\
--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,60 @@
-#include <Kokkos_Core.hpp>
-#include<cstdio>
-#include <chrono>
-#include <ctime>
+#include <CL/sycl.hpp>
 #include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 
+using namespace sycl;
 
-#define MyBackend Kokkos::OpenMP
-#define MyDeviceMemorySpace Kokkos::HostSpace
-#define MyHostMemorySpace Kokkos::HostSpace
-#define MyUSpace Kokkos::HostSpace
+int main(int argc, char *argv[]) {
+  // Initialize SYCL queue with OpenMP backend
+  queue q{cpu_selector{}, property::queue::in_order()};
+  srand(time(0));
 
-#define MyView(Type) Kokkos::View<Type *, MyDeviceMemorySpace>
-#define MyUnmanagedHostView(Type) Kokkos::View<Type *, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged>>
-#define MyUnmanagedView(Type) Kokkos::View<Type *, MyDeviceMemorySpace, Kokkos::MemoryTraits<Kokkos::Unmanaged>>
+  int Ncell = 10000;
+  int VertPerCell = 642;
+  int size = Ncell * VertPerCell;
+  double *array = (double *)malloc(size * sizeof(double));
 
+  // Allocate memory in SYCL
+  double *reuseableTempMemory = malloc_shared<double>(size, q);
+  double *arrayDeviceView = malloc_shared<double>(size, q);
 
-
-int main(int argc, char* argv[]) {
-  Kokkos::initialize(argc,argv);
-  {
-   srand(time(0));
-   int Ncell=10000;
-   int VertPerCell=642;
-   int size=Ncell*VertPerCell;
-   double *array = (double*)malloc(size*sizeof(double));
-   MyView(double) reuseableTempMemory("reusableTempMemory",size);
-   for (int i = 0; i < size; ++i) {
-        array[i] = double(rand() % 100)/100.0;
-   }
-
-   MyUnmanagedHostView(double) arrayView(array,size);
-
-   MyView(double) arrayDeviceView("arrayDeviceView",size);
-
-   Kokkos::deep_copy(arrayDeviceView,arrayView);
-
-
-//   int *cellDel=(int*)malloc(100*sizeof(int));
-//   for (int i=0; i<100; ++i){
-//	cellDel[i] = rand() % Ncell + 1;
-//   }
-   
-   auto start = std::chrono::high_resolution_clock::now();   
-   for (int i=0; i<2030; ++i){//about 2k cells being deleted in HARVEY
-        int nIndsToDelete = 642; //same as nVertsToDelete
-        int startIndex = 0; // same as startVertex
-        int preNIndices = Ncell*VertPerCell; //same as totalNVertices
-        int nDataToMove= preNIndices - (startIndex + nIndsToDelete);//10000000;//(Ncell-cellDel[i])*VertPerCell;
-	double* dstPtr = arrayDeviceView.data()+startIndex;
-	double* srcPtr = arrayDeviceView.data()+startIndex + nIndsToDelete;
-	double* tmpBufferPtr = reuseableTempMemory.data();
-        MyUnmanagedView(double) tmpBufferPtrView((double*)tmpBufferPtr,nDataToMove);
-        MyUnmanagedView(double) dstPtrView(dstPtr,nDataToMove);
-        MyUnmanagedView(double) srcPtrView(srcPtr,nDataToMove);
-        Kokkos::deep_copy(tmpBufferPtrView,srcPtrView);
-        Kokkos::deep_copy(dstPtrView,tmpBufferPtrView);
-	}
-   auto end = std::chrono::high_resolution_clock::now();
-   std::chrono::duration<double> duration = end - start;
-   double timeInSeconds = duration.count();
-
-   std::cout << "Runtime (sec): " << timeInSeconds << std::endl;
-   free(array);
+  // Initialize the array with random values
+  for (int i = 0; i < size; ++i) {
+    array[i] = double(rand() % 100) / 100.0;
   }
-  Kokkos::finalize();
+
+  // Copy data from host array to device memory
+  q.memcpy(arrayDeviceView, array, size * sizeof(double)).wait();
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (int i = 0; i < 2030; ++i) { // about 2k cells being deleted in HARVEY
+    int nIndsToDelete = 642; // same as nVertsToDelete
+    int startIndex = 0;      // same as startVertex
+    int preNIndices = Ncell * VertPerCell; // same as totalNVertices
+    int nDataToMove = preNIndices - (startIndex + nIndsToDelete);
+
+    double *dstPtr = arrayDeviceView + startIndex;
+    double *srcPtr = arrayDeviceView + startIndex + nIndsToDelete;
+    double *tmpBufferPtr = reuseableTempMemory;
+
+    // Copy data using SYCL queue
+    q.memcpy(tmpBufferPtr, srcPtr, nDataToMove * sizeof(double)).wait();
+    q.memcpy(dstPtr, tmpBufferPtr, nDataToMove * sizeof(double)).wait();
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  double timeInSeconds = duration.count();
+
+  std::cout << "Runtime (sec): " << timeInSeconds << std::endl;
+
+  // Free allocated memory
+  free(array);
+  free(reuseableTempMemory, q);
+  free(arrayDeviceView, q);
+
+  return 0;
 }
